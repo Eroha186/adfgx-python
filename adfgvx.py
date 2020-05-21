171680d087eaad5c21d1936588599ec8b4ef561f
finloukuritsun2090 <finloukuritsun2090@yandex.com>
2020-05-22 02:48:55 +0700

    Реализация функций чтения и записи файла

--- adfgvx.py
+++ adfgvx.py
@@ -1,5 +1,5 @@
 import sys
-from encryption import main
+from encryption import main, read_file, write_file
 
 command = sys.argv[1]
 
@@ -7,7 +7,10 @@ if command == 'cripto':
     title = sys.argv[2]
     key = sys.argv[3]
     table_key = sys.argv[4]
-    chipher = main(title, 'привет дорогой друг', key, table_key)
-    print(chipher)
+    file_text = sys.argv[5]
+    cipher = main(title, read_file(file_text), key, table_key)
+
+    write_file('encryption.txt', cipher)
+    print(cipher)
 elif command == 'help':
     pass
